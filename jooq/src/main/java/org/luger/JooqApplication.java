288150597fa596eea6ced8deb25e795e4a34c8bf
Gerrardinho <gerrardinho.08@gmail.com>
2017-02-25 15:14:14 -0600

    initial commit

---
+++ jooq/src/main/java/org/luger/JooqApplication.java
@@ -0,0 +1,16 @@
+package org.luger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by gerardo8 on 25/02/2017.
+ */
+@SpringBootApplication
+public class JooqApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqApplication.class, args);
+    }
+}
