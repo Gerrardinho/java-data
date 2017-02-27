775e849bb7d7b6199f7cefe0041e7b06c5359f21
Gerrardinho <gerrardinho.08@gmail.com>
2017-02-26 20:51:27 -0600

    spring-data-rest finished

--- spring-data-jpa/src/main/java/org/luger/SpringDataJpaApplication.java
+++ spring-data-jpa/src/main/java/org/luger/SpringDataJpaApplication.java
@@ -1,13 +1,37 @@
 package org.luger;
 
+import org.luger.domain.Person;
+import org.luger.domain.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by gerardo8 on 25/02/2017.
  */
 @SpringBootApplication
 public class SpringDataJpaApplication {
+
+    @Bean
+    CommandLineRunner runner(PersonRepository personRepository) {
+        return args -> {
+
+            Person person1 = new Person("Christopher", "Wallace", "wallace@gmail.com");
+            Person person2 = new Person("Lesane", "Crooks", "crooks@gmail.com");
+            Person person3 = new Person("Marshall", "Mathers", "marshall@gmail.com");
+
+            personRepository.save(person1);
+            personRepository.save(person2);
+            personRepository.save(person3);
+
+            person1.getFriends().add(person2);
+            person1.getFriends().add(person3);
+
+            personRepository.save(person1);
+
+        };
+    }
 
 
     public static void main(String[] args) {
