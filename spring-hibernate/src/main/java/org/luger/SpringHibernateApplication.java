562629ebd8fc5095c03896ff8dac321cabc6f232
Gerrardinho <gerrardinho.08@gmail.com>
2017-03-05 14:58:52 -0600

    spring hibernate

--- spring-hibernate/src/main/java/org/luger/SpringHibernateApplication.java
+++ spring-hibernate/src/main/java/org/luger/SpringHibernateApplication.java
@@ -1,7 +1,11 @@
 package org.luger;
 
+import org.luger.model.Person;
+import org.luger.service.PersonService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by gerardo8 on 25/02/2017.
@@ -9,6 +13,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringHibernateApplication {
 
+    private final PersonService personService;
+
+    public SpringHibernateApplication(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            personService.save(new Person("Gerardo", "López", "gerardo.lopezre@gmail.com"));
+            personService.save(new Person("Steven", "Gerrard", "steven.gerrard@gmail.com"));
+            personService.save(new Person("Frank", "Lampard", "frank.lampard@gmail.com"));
+
+            Person person = personService.findByEmail("gerardo.lopezre@gmail.com");
+
+            System.out.println(person);
+
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringHibernateApplication.class, args);
